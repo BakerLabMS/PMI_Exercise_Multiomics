3f3f20a2d3e08a0e3654a8b04b0ff25a47ffa0d1
Jrchapp3 <jrchapp3@ncsu.edu>
2024-03-15 14:18:10 -0400

    initial commit

---
+++ PMI_EX_analyses/PMI_lipid_SMILES.R
@@ -0,0 +1,30 @@
+library("httr")
+library("jsonlite")
+library(fingerprint)
+library(rcdk)
+library(rprojroot)
+
+setwd(find_root("Github_PMI_Exercise.Rproj"))
+setwd("PMI_EX_analyses/PMI_EX_data")
+
+
+df <- read.csv("PMI_Lipid_SMILES.csv", stringsAsFactors = F)
+
+head(df)
+
+sum(df$smi == "")
+
+df.c <- df[df$smi != "", ]
+df.na <- df[df$smi == "", ]
+
+head(df.c)
+
+fing <- parse.smiles(df.c$smi)
+fing <- lapply(fing, get.fingerprint, type = "circular")
+fp.sim <- fp.sim.matrix(fing)
+row.names(fp.sim) <- df.c$abbrev
+fp.dist <- as.dist(1 - fp.sim)
+cls <- hclust(fp.dist)
+plot(cls)
+
+save.image("fp_PMI_All lipids.rdata")
